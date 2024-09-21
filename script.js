7a3605a898191740151f69c4967ba103d476f828
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:29:40 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+// Test cases
+console.log(removeDuplicates([1, 0, 1, 0]));               
+console.log(removeDuplicates(["New", "York", "City"]));     
+console.log(removeDuplicates(["Hassan", "Aisha", "Aisha"]));
